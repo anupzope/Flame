f35a31f9d2db6958f2d1d310787c2c32554e134b
Anup Zope <anupzope@gmail.com>
2023-10-06 09:15:44 -0500

    Modified script that installs dependencies.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -13,10 +13,10 @@
 #PATH=/cavs/projects/nasafsi_codes/adz8/projects/Flame/build-warhawk1/LFlame3:$PATH
 
 # On Rifle
-module load zlib/1.2.13 gcc/12.2.0 mpich/4.0.2
-BDIR="build-rifle"
-IPATH=/var/tmp/anup/flame-deps
-MPATH=/var/tmp/anup/flame-deps/modulefiles
+#module load zlib/1.2.13 gcc/12.2.0 mpich/4.0.2
+#BDIR="build-rifle"
+#IPATH=/var/tmp/anup/flame-deps
+#MPATH=/var/tmp/anup/flame-deps/modulefiles
 
 # On Guppy
 # module load zlib/1.2.12 gcc/12.1.0 mpich/4.0.2
@@ -24,8 +24,16 @@ MPATH=/var/tmp/anup/flame-deps/modulefiles
 # IPATH=/var/tmp/anup/flame-deps
 # MPATH=/var/tmp/anup/flame-deps/modulefiles
 
+# On Orion
+module load zlib/1.2.11 gcc/11.3.0 mpich/3.3.2
+BDIR="build-orion"
+IPATH=/apps/contrib/FSI/Flame-Stack/01
+MPATH=/apps/contrib/FSI/Flame-Stack/01/modulefiles
+
 # Additional dependencies required for building Flame dependencies:
 module load cmake texlive python
+
+module list
 
 # Get MPI Configuration
 MPICC=`/usr/bin/which --tty-only mpicc`
@@ -84,11 +92,20 @@ fi
 ## Install GKlib, METIS, and ParMETIS ##
 ########################################
 
-# Obtain source from: git clone https://github.com/KarypisLab/GKlib.git. Edit
-# the `CMakeLists.txt` file in GKlib and turn `ON` the `BUILD_SHARED_LIBS`
-# options after cloning the repository. Add Compiler flag
-# "-D_POSIX_C_SOURCE=199309L" in GKlib and ParMETIS to avoid error related to
-# siginfo_t.
+# Obtain source from:
+# git clone https://github.com/KarypisLab/GKlib.git.
+# git clone https://github.com/KarypisLab/ParMETIS.git
+# git clone https://github.com/KarypisLab/METIS.git
+# 
+# Edit
+# the `CMakeLists.txt` file in GKlib and turn `ON` the
+# `BUILD_SHARED_LIBS` options after cloning the repository.
+# 
+# Add Compiler flag "-D_POSIX_C_SOURCE=199309L" in GKlib,
+# METIS, and ParMETIS (as follows) to avoid error related
+# to siginfo_t.
+# 
+# add_compile_definitions(_POSIX_C_SOURCE=199309L)
 
 INSTALL_METIS="1"
 METIS_VERSION="master"
@@ -101,7 +118,7 @@ if [[ ${INSTALL_METIS} -eq 1 ]]; then
   cp -R ../sources/GKlib ./
   cd GKlib && echo "Directory: ${PWD}"
   make config prefix="${METIS_IPATH}"
-  make CPPFLAGS="-D_POSIX_C_SOURCE=199309L" -j4
+  make -j4
   make install
   cd .. && echo "Directory: ${PWD}"
   cd .. && echo "Directory: ${PWD}"
@@ -110,7 +127,7 @@ if [[ ${INSTALL_METIS} -eq 1 ]]; then
   rm -rf METIS
   cp -R ../sources/METIS ./
   cd METIS && echo "Directory: ${PWD}"
-  make config prefix="${METIS_IPATH}" shared=1
+  make config prefix="${METIS_IPATH}" gklib_path="${METIS_IPATH}" shared=1
   make -j4
   make install
   cd .. && echo "Directory: ${PWD}"
@@ -120,7 +137,7 @@ if [[ ${INSTALL_METIS} -eq 1 ]]; then
   rm -rf ParMETIS
   cp -R ../sources/ParMETIS ./
   cd ParMETIS && echo "Directory: ${PWD}"
-  make config prefix="${METIS_IPATH}" shared=1
+  make config prefix="${METIS_IPATH}" gklib_path="${METIS_IPATH}" shared=1
   make -j4
   make install
   cd .. && echo "Directory: ${PWD}"
@@ -166,12 +183,16 @@ if [[ ${INSTALL_HDF5} -eq 1 ]]; then
   echo "Extracting source for HDF5..."
   tar -xf ../sources/hdf5-1.14.1-2.tar.gz
   cd hdf5-1.14.1-2 && echo "Directory: ${PWD}"
+  mkdir build
+  cd build && echo "Directory: ${PWD}"
   echo "Configuring HDF5..."
-  CFLAGS="${MPI_CFLAGS}" CXXFLAGS="${MPI_CFLAGS}" LDFLAGS="${MPI_LDFLAGS}" ./configure --enable-parallel --prefix="${HDF5_IPATH}"
+  cmake .. -DHDF5_ENABLE_PARALLEL=ON -DCMAKE_INSTALL_PREFIX="${HDF5_IPATH}"
+  #CFLAGS="${MPI_CFLAGS}" CXXFLAGS="${MPI_CFLAGS}" LDFLAGS="${MPI_LDFLAGS}" ./configure --enable-parallel --prefix="${HDF5_IPATH}"
   echo "Building HDF5..."
   make -j4
   echo "Installing HDF5..."
   make install
+  cd .. && echo "Directory: ${PWD}"
   cd .. && echo "Directory: ${PWD}"
   cd .. && echo "Directory: ${PWD}"
   
@@ -202,6 +223,9 @@ module load "${HDF5_MODULENAME}/${HDF5_VERSION}"
 ## Install CGNS ##
 ##################
 
+# Obtain source from:
+# git clone -b master https://github.com/CGNS/CGNS.git
+
 INSTALL_CGNS="1"
 CGNS_VERSION="4.3.0"
 CGNS_MODULENAME="FlameCGNS"
@@ -218,7 +242,7 @@ if [[ ${INSTALL_CGNS} -eq 1 ]]; then
   cd build && echo "Directory: ${PWD}"
   echo "Configuring CGNS..."
   cmake -DCGNS_BUILD_SHARED=ON -DCMAKE_INSTALL_PREFIX="${CGNS_IPATH}" \
-    -DHDF5_DIR="${HDF5_IPATH}" -DHDF5_NEED_MPI=ON -DHDF5_NEED_ZLIB=ON  -DCGNS_ENABLE_PARALLEL=ON ..
+    -DHDF5_NEED_MPI=ON -DHDF5_NEED_ZLIB=ON  -DCGNS_ENABLE_PARALLEL=ON ..
   echo "Building CGNS..."
   make -j4
   echo "Installing CGNS..."
@@ -378,7 +402,7 @@ if [[ ${INSTALL_LOCI} -eq 1 ]]; then
   cp -R ../sources/Loci ./
   cd Loci && echo "Directory: ${PWD}"
   echo "Configuring Loci..."
-  ./configure --prefix "${LOCI_IPATH}" --with-metis "${METIS_IPATH}"
+  ./configure --prefix "${LOCI_IPATH}" --with-metis "${METIS_IPATH}" --with-mpi "${MPI_DIR}"
   echo "Building Loci..."
   make -j4
   echo "Installing Loci..."
